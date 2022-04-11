d9d3609704e6989802327444a83e444a4d29ab5e
emily <emilyvicki601@gmail.com>
2022-04-11 12:11:32 +0800

    feat: add capsules, letters entities

---
+++ app/db/migrations/002_letters_create.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+require 'sequel'
+
+Sequel.migration do
+  change do
+    create_table(:letters) do
+      primary_key :id
+      foreign_key :capsule_id, table: :capsules
+
+      # String :member_id, null: false, default: ''
+      String :receiver_id, null: false
+      String :title, null: false
+      String :content, null: false, default: ''
+
+      DateTime :send_at
+      DateTime :open_at
+      DateTime :created_at
+
+      # 1: draft, 2: ready to be send, 3: sent
+      Integer :status, null: false, default: 1
+
+      TrueClass :is_private, null: false, default: true
+    end
+  end
+end

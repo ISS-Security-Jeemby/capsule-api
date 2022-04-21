63e4739d7d3806b0811e1919cfe43eae7ababde5
Jing Han, Huang <7jesstc1999@gmail.com>
2022-04-21 16:50:40 +0800

    test: add warn/error logs in app.rb

--- app/controllers/app.rb
+++ app/controllers/app.rb
@@ -37,6 +37,7 @@ module TimeCapsule
                 letter = Letter.where(capsule_id: caps_id, id: let_id).first
                 letter ? letter.to_json : raise('Letter not found')
               rescue StandardError => e
+                Api.logger.warn "LETTER NOT FOUND: CAPS_ID - #{caps_id} / LAT_ID - #{let_id}"
                 routing.halt 404, { message: e.message }.to_json
               end
 
@@ -45,6 +46,7 @@ module TimeCapsule
                 output = { data: Capsule.first(id: caps_id).letters }
                 JSON.pretty_generate(output)
               rescue StandardError
+                Api.logger.warn "LETTERS NOT FOUND: CAPS_ID - #{caps_id}"
                 routing.halt 404, message: 'Could not find letters'
               end
 
@@ -61,7 +63,8 @@ module TimeCapsule
               rescue Sequel::MassAssignmentRestriction
                 Api.logger.warn "MASS-ASSIGNMENT: #{new_data.keys}"
                 routing.halt 400, { message: 'Illegal Attributes' }.to_json
-              rescue StandardError
+              rescue StandardError  => e
+                Api.logger.error "UNKOWN ERROR: #{e.message}"
                 routing.halt 500, { message: e.message }.to_json
               end
             end
@@ -71,6 +74,7 @@ module TimeCapsule
               caps = Capsule.first(id: caps_id)
               caps ? caps.to_json : raise('Capsule not found')
             rescue StandardError => e
+              Api.logger.warn "CAPSULE NOT FOUND: #{caps_id}"
               routing.halt 404, { message: e.message }.to_json
             end
           end
@@ -79,7 +83,8 @@ module TimeCapsule
           routing.get do
             output = { data: Capsule.all }
             JSON.pretty_generate(output)
-          rescue StandardError
+          rescue StandardError => e
+            Api.logger.warn "ALL CAPSULES NOT FOUND: #{e.message}"
             routing.halt 404, { message: 'Could not find capsules' }.to_json
           end
 

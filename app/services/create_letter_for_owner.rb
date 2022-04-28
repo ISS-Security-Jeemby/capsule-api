86fda25fcb82ff35a8957e7e39d9f8309e5577e3
emily <emilyvicki601@gmail.com>
2022-04-28 23:00:21 +0800

    feat: add services

--- app/services/create_letter_for_owner.rb
+++ app/services/create_letter_for_owner.rb
@@ -2,9 +2,10 @@
 
 module TimeCapsule
   # Create new configuration for a capsule
-  class CreateLetterForCapsule
+  class CreateLetterForOwner
     def self.call(capsule_id:, letter_data:)
-      Capsule.first(id: capsule_id).add_letter(letter_data)
+      Capsule.first(id: capsule_id)
+             .add_owned_letter(letter_data)
     end
   end
 end

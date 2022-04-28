65823242ecfbbee8a7648bb9f97bbf07d13e666d
emily <emilyvicki601@gmail.com>
2022-04-28 22:57:18 +0800

    fix: letter_id type as uuid

--- app/db/migrations/004_capsules_letters_join_create.rb
+++ app/db/migrations/004_capsules_letters_join_create.rb
@@ -4,6 +4,6 @@ require 'sequel'
 
 Sequel.migration do
   change do
-    create_join_table(collaborator_id: :accounts, letter_id: :letters)
+    create_join_table(capsule_id: :capsules, letter_id: { table: :letters, type: :uuid })
   end
 end

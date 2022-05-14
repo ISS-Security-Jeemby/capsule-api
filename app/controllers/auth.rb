a64d82c9bc35d356fadcca28c89690c09abe6e5e
Jing Han, Huang <7jesstc1999@gmail.com>
2022-05-14 18:04:00 +0800

    feat: update register routing

--- app/controllers/auth.rb
+++ app/controllers/auth.rb
@@ -10,15 +10,17 @@ module TimeCapsule
       routing.on 'register' do
         # POST api/v1/auth/register
         routing.post do
-          VerifyRegistration.new(Api.config, @request_data).call
+          reg_data = JsonRequestBody.parse_symbolize(request.body.read)
+          VerifyRegistration.new(reg_data).call
 
           response.status = 202
           { message: 'Verification email sent' }.to_json
         rescue VerifyRegistration::InvalidRegistration => e
           routing.halt 400, { message: e.message }.to_json
+        rescue VerifyRegistration::EmailProviderError
+          routing.halt 500, { message: 'Error sending email' }.to_json
         rescue StandardError => e
-          puts "ERROR VERIFYING REGISTRATION: #{e.inspect}"
-          puts e.message
+          Api.logger.error "Could not verify registration: #{e.inspect}"
           routing.halt 500
         end
       end

23c53b6fde78bc76b27cb783f81612e9f5c70294
Jing Han, Huang <7jesstc1999@gmail.com>
2022-05-26 19:23:45 +0800

    feat: policies updates and adds policy summary

--- app/policies/capsule_policy.rb
+++ app/policies/capsule_policy.rb
@@ -1,31 +1,72 @@
 # frozen_string_literal: true
 
-# Policy to determine if account can view a letter
-class CapsulePolicy
-  def initialize(account, capsule)
-    @account = account
-    @capsule = capsule
-  end
+module TimeCapsule
+  # Policy to determine if an account can view a particular capsule
+  class CapsulePolicy
+    def initialize(account, capsule, letter = nil)
+      @account = account
+      @capsule = capsule
+      @letter = letter
+    end
 
-  def can_view?
-    account_owns_letter? || account_collaborates_on_letter?
-  end
+    def can_view?
+      account_is_owner? || account_is_collaborator?
+    end
 
-  def can_edit?
-    account_owns_letter? || account_collaborates_on_letter?
-  end
+    def can_edit?
+      account_is_owner? || account_is_collaborator?
+    end
 
-  def can_delete?
-    account_owns_letter? || account_collaborates_on_letter?
-  end
+    def can_delete?
+      account_is_owner?
+    end
 
-  private
+    def can_leave?
+      account_is_collaborator?
+    end
 
-  def account_owns_letter?
-    @capsule.letter.owner == @account
-  end
+    def can_add_letters?
+      account_is_owner? || account_is_collaborator?
+    end
 
-  def account_collaborates_on_letter?
-    @capsule.letter.collaborators.include?(@account)
+    def can_remove_letters?
+      account_is_owner? || account_is_collaborator?
+    end
+
+    def can_add_collaborators?
+      account_is_owner?
+    end
+
+    def can_remove_collaborators?
+      account_is_owner?
+    end
+
+    def can_collaborate?
+      !(account_is_owner? or account_is_collaborator?)
+    end
+
+    def summary
+      {
+        can_view: can_view?,
+        can_edit: can_edit?,
+        can_delete: can_delete?,
+        can_leave: can_leave?,
+        can_add_documents: can_add_documents?,
+        can_delete_documents: can_remove_documents?,
+        can_add_collaborators: can_add_collaborators?,
+        can_remove_collaborators: can_remove_collaborators?,
+        can_collaborate: can_collaborate?
+      }
+    end
+
+    private
+
+    def account_is_owner?
+      @capsule.owner == @account
+    end
+
+    def account_is_collaborator?
+      @capsule.collaborated_letters.include?(@letter)
+    end
   end
 end
--- app/policies/letter_policy.rb
+++ app/policies/letter_policy.rb
@@ -1,57 +1,39 @@
 # frozen_string_literal: true
 
-module TimeCapsule
-  # Policy to determine if an account can view a particular letter
-  class LetterPolicy
-    def initialize(account, letter)
-      @account = account
-      @letter = letter
-    end
+# Policy to determine if an account can view a particular letter
+class LetterPolicy
+  def initialize(account, letter)
+    @account = account
+    @letter = letter
+  end
 
-    def can_view?
-      account_is_owner? || account_is_collaborator?
-    end
+  def can_view?
+    account_owns_capsule? || account_collaborates_on_capsule?
+  end
 
-    def can_edit?
-      account_is_owner? || account_is_collaborator?
-    end
+  def can_edit?
+    account_owns_capsule? || account_collaborates_on_capsule?
+  end
 
-    def can_delete?
-      account_is_owner?
-    end
+  def can_delete?
+    account_owns_capsule? || account_collaborates_on_capsule?
+  end
 
-    def can_leave?
-      account_is_collaborator?
-    end
+  def summary
+    {
+      can_view: can_view?,
+      can_edit: can_edit?,
+      can_delete: can_delete?
+    }
+  end
 
-    def can_add_documents?
-      account_is_owner? || account_is_collaborator?
-    end
+  private
 
-    def can_remove_documents?
-      account_is_owner? || account_is_collaborator?
-    end
+  def account_owns_capsule?
+    @letter.capsule.owner == @account
+  end
 
-    def can_add_collaborators?
-      account_is_owner?
-    end
-
-    def can_remove_collaborators?
-      account_is_owner?
-    end
-
-    def can_collaborate?
-      !(account_is_owner? or account_is_collaborator?)
-    end
-
-    private
-
-    def account_is_owner?
-      @letter.owner == @account
-    end
-
-    def account_is_collaborator?
-      @letter.collaborators.include?(@account)
-    end
+  def account_collaborates_on_capsule?
+    @letter.capsule.collaborators.include?(@account)
   end
 end

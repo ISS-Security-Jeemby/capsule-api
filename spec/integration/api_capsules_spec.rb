448b93e15696bd02d803759a8f2aa5ecad4cae70
emily <emilyvicki601@gmail.com>
2022-05-31 23:31:04 +0800

    test: pass capsules' test

--- spec/integration/api_capsules_spec.rb
+++ spec/integration/api_capsules_spec.rb
@@ -8,7 +8,10 @@ describe 'Test Capsule Handling' do
   before do
     wipe_database
     @account_data = DATA[:accounts][0]
+    @wrong_account_data = DATA[:accounts][1]
+
     @account = TimeCapsule::Account.create(@account_data)
+    @wrong_account = TimeCapsule::Account.create(@wrong_account_data)
 
     header 'CONTENT_TYPE', 'application/json'
   end
@@ -30,7 +33,6 @@ describe 'Test Capsule Handling' do
       end
 
       it 'BAD: should not process for unauthorized account' do
-        header 'AUTHORIZATION', 'Bearer bad_token'
         get 'api/v1/capsules'
         _(last_response.status).must_equal 403
 
@@ -40,54 +42,50 @@ describe 'Test Capsule Handling' do
     end
 
     it 'HAPPY: should be able to get details of a single Capsule' do
-      existing_capsule = DATA[:capsules][1]
-      TimeCapsule::Capsule.create(existing_capsule).save
-      id = TimeCapsule::Capsule.first.id
+      capsule = @account.add_owned_capsule(DATA[:capsules][0])
 
-      get "/api/v1/capsules/#{id}"
+      header 'AUTHORIZATION', auth_header(@account_data)
+      get "/api/v1/capsules/#{capsule.id}"
       _(last_response.status).must_equal 200
-
-      result = JSON.parse last_response.body
-      _(result['data']['attributes']['id']).must_equal id
-      _(result['data']['attributes']['name']).must_equal existing_capsule['name']
+      result = JSON.parse(last_response.body)['data']
+      _(result['attributes']['id']).must_equal capsule.id
+      _(result['attributes']['name']).must_equal capsule.name
     end
 
-    # it 'SAD: should return error if unknown Capsule requested' do
-    #   header 'AUTHORIZATION', auth_header(@account_data)
-    #   get '/api/v1/capsules/foobar'
+    it 'SAD: should return error if unknown Capsule requested' do
+      header 'AUTHORIZATION', auth_header(@account_data)
+      get '/api/v1/capsules/foobar'
 
-    #   _(last_response.status).must_equal 404
-    # end
+      _(last_response.status).must_equal 404
+    end
 
-    # it 'SECURITY: should prevent basic SQL injection targeting IDs' do
-    # TimeCapsule::Capsule.create(name: 'New Capsule')
-    #  TimeCapsule::Capsule.create(name: 'Newer Capsule')
-    # get 'api/v1/capsules/2%20or%20id%3E0'
+    it 'BAD AUTHORIZATION: should not get capsule with wrong authorization' do
+      capsule = @account.add_owned_capsule(DATA[:capsules][0])
 
-    #   # deliberately not reporting error -- don't give attacker information
-    #   _(last_response.status).must_equal 404
-    #   _(last_response.body['data']).must_be_nil
-    # end
+      header 'AUTHORIZATION', auth_header(@wrong_account_data)
+      get "/api/v1/capsules/#{capsule.id}"
+      _(last_response.status).must_equal 403
+
+      result = JSON.parse last_response.body
+      _(result['attributes']).must_be_nil
+    end
+
+    it 'BAD SQL VULNERABILTY: should prevent basic SQL injection of id' do
+      @account.add_owned_capsule(DATA[:capsules][0])
+      @account.add_owned_capsule(DATA[:capsules][1])
+
+      header 'AUTHORIZATION', auth_header(@account_data)
+      get 'api/v1/capsules/2%20or%20id%3E0'
+
+      # deliberately not reporting detection -- don't give attacker information
+      _(last_response.status).must_equal 404
+      _(last_response.body['data']).must_be_nil
+    end
   end
+
   describe 'Creating New Capsules' do
     before do
       @capsule_data = DATA[:capsules][0]
     end
-
-    # it 'HAPPY: should be able to create new Capsules for owner' do
-    #   header 'AUTHORIZATION', auth_header(@account_data)
-    #   post 'api/v1/capsules', @capsule_data.to_json
-    #   binding.irb
-
-    #   _(last_response.status).must_equal 201
-    #   _(last_response.header['Location'].size).must_be :>, 0
-
-    #   created_capsules = JSON.parse(last_response.body)['data'][0]['attributes']
-    #   capsule = TimeCapsule::Capsule.first
-
-    #   _(created_capsules['id']).must_equal capsule.id
-    #   _(created_capsules['name']).must_equal capsule.name
-    #   _(created_capsules['type']).must_equal capsule.type
-    # end
   end
 end

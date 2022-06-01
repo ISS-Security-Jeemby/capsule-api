d1ffa4a23cd0aa56271f1ddaac55f9761bc076d7
ruby2800 <36958049+ruby2800@users.noreply.github.com>
2022-06-01 20:46:26 +0800

    style: follow rubocop

--- app/controllers/auth.rb
+++ app/controllers/auth.rb
@@ -51,14 +51,14 @@ module TimeCapsule
       # POST /api/v1/auth/google_sso
       routing.post 'google_sso' do
         auth_request = JsonRequestBody.parse_symbolize(request.body.read).to_json
-        auth_request_token=JSON.parse(auth_request)["access_token"]
+        auth_request_token = JSON.parse(auth_request)['access_token']
         auth_account = AuthorizeGoogleSso.new.call(auth_request_token)
         { data: auth_account }.to_json
       rescue StandardError => e
         puts "FAILED to validate Google account: #{e.inspect}"
         puts e.backtrace
         routing.halt 400
-      end     
+      end
     end
   end
 end

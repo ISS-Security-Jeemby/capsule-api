69e9771787bb24d881f817d53024899fdb63ca4e
emily <emilyvicki601@gmail.com>
2022-06-15 16:25:09 +0800

    feat: add shared_capsules controller

--- app/controllers/capsules.rb
+++ app/controllers/capsules.rb
@@ -38,6 +38,24 @@ module TimeCapsule
               puts e.full_message
               routing.halt 404, { message: e.message }.to_json
             rescue StandardError => e
+              puts e.full_message
+              puts "FIND CAPSULE ERROR: #{e.inspect}"
+              routing.halt 500, { message: 'API server error' }.to_json
+            end
+          end
+
+          routing.is 'shared' do
+            # GET api/v1/capsules/[caps_id]/letters/shared
+            routing.get do
+              letters = LetterPolicy::AccountScope.new(@auth, @auth_account).viewable
+              JSON.pretty_generate(data: letters)
+            rescue GetCapsuleQuery::ForbiddenError => e
+              puts e.full_message
+              routing.halt 403, { message: e.message }.to_json
+            rescue GetCapsuleQuery::NotFoundError => e
+              puts e.full_message
+              routing.halt 404, { message: e.message }.to_json
+            rescue StandardError => e
               puts "FIND CAPSULE ERROR: #{e.inspect}"
               routing.halt 500, { message: 'API server error' }.to_json
             end

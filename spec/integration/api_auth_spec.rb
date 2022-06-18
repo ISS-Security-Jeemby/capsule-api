1a010da8df7220df1db7064c8c8e1e116e6b9861
ruby2800 <36958049+ruby2800@users.noreply.github.com>
2022-06-18 15:18:33 +0800

    test: test google sso

--- spec/integration/api_auth_spec.rb
+++ spec/integration/api_auth_spec.rb
@@ -47,7 +47,7 @@ describe 'Test Authentication Routes' do
     end
   end
 
- describe 'SSO Authorization' do
+  describe 'GITHUB SSO Authorization' do
     before do
       WebMock.enable!
       WebMock.stub_request(:get, app.config.GITHUB_ACCOUNT_URL)
@@ -60,7 +60,7 @@ describe 'Test Authentication Routes' do
       WebMock.disable!
     end
 
-    it 'HAPPY AUTH SSO: should authenticate+authorize new valid SSO account' do
+    it 'HAPPY AUTH SSO: should authenticate+authorize new valid GITHUB SSO account' do
       gh_access_token = { access_token: GOOD_GH_ACCESS_TOKEN }
 
       post 'api/v1/auth/sso',
@@ -76,7 +76,7 @@ describe 'Test Authentication Routes' do
       _(account['id']).must_be_nil
     end
 
-    it 'HAPPY AUTH SSO: should authorize existing SSO account' do
+    it 'HAPPY AUTH SSO: should authorize existing GITHUBSSO account' do
 
       wipe_database
       
@@ -96,6 +96,61 @@ describe 'Test Authentication Routes' do
       _(last_response.status).must_equal 200
       _(account['username']).must_equal(GH_SSO_ACCOUNT['gh_sso_username'])
       _(account['email']).must_equal(GH_SSO_ACCOUNT['gh_email'])
+      _(account['id']).must_be_nil
+    end
+  end
+
+  describe 'Google SSO Authorization' do
+    before do
+      WebMock.enable!
+      WebMock.stub_request(:get, app.config.GOOGLE_ACCOUNT_URL)
+             .to_return(body: GOOGLE_ACCOUNT_RESPONSE[GOOD_GOOGLE_ACCESS_TOKEN],
+                        status: 200,
+                        headers: { 'content-type' => 'application/json' })
+    end
+
+    after do
+      WebMock.disable!
+    end
+
+    it 'HAPPY AUTH GOOGLE SSO: should authenticate+authorize new valid GOOGLE SSO account' do
+      google_access_token = { access_token: GOOD_GOOGLE_ACCESS_TOKEN }
+
+      post 'api/v1/auth/google_sso',
+           SignedRequest.new(app.config).sign(google_access_token).to_json,
+           @req_header
+
+      auth_account = JSON.parse(last_response.body)['data']
+      account = auth_account['attributes']['account']
+
+      _(last_response.status).must_equal 200
+      _(account['username']).must_equal(GH_SSO_ACCOUNT['gh_sso_username'])
+      _(account['email']).must_equal(GH_SSO_ACCOUNT['gh_email'])
+      _(account['id']).must_be_nil
+    end
+
+    it 'HAPPY AUTH GOOGLE SSO: should authorize existing GOOGLE SSO account' do
+
+      wipe_database
+      
+
+      TimeCapsule::Account.create(
+        username: GOOGLE_SSO_ACCOUNT['google_sso_username'],
+        email: GOOGLE_SSO_ACCOUNT['google_email']
+      )
+      google_access_token = { access_token: GOOD_GOOGLE_ACCESS_TOKEN }
+
+     
+      post 'api/v1/auth/google_sso',
+           SignedRequest.new(app.config).sign(google_access_token).to_json,
+           @req_header
+
+      auth_account = JSON.parse(last_response.body)['data']
+      account = auth_account['attributes']['account']
+      
+      _(last_response.status).must_equal 200
+      _(account['username']).must_equal(GOOGLE_SSO_ACCOUNT['google_sso_username'])
+      _(account['email']).must_equal(GOOGLE_SSO_ACCOUNT['google_email'])
       _(account['id']).must_be_nil
     end
   end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -52,3 +52,10 @@ GH_ACCOUNT_RESPONSE = YAML.load(
 )
 GOOD_GH_ACCESS_TOKEN = GH_ACCOUNT_RESPONSE.keys.first
 GH_SSO_ACCOUNT = YAML.load(File.read('spec/fixtures/gh_sso_account.yml'))
+
+## Google SSO fixtures
+GOOGLE_ACCOUNT_RESPONSE = YAML.load(
+  File.read('spec/fixtures/google_token_response.yml')
+)
+GOOD_GOOGLE_ACCESS_TOKEN = GOOGLE_ACCOUNT_RESPONSE.keys.first
+GOOGLE_SSO_ACCOUNT = YAML.load(File.read('spec/fixtures/google_sso_account.yml'))
